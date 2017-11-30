d169069e08595fa4a6128e5c77e72453a308b5c3
Jake Becker <jake@jakebecker.org>
2017-11-29 16:44:04 -0800

    Remove wordPattern which strangely causes autocomplete not to fire

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,7 +1,6 @@
 import * as vscode from 'vscode';
 
 export const configuration: vscode.LanguageConfiguration = {
-  wordPattern: /:?\w+[\.\w+]*/g,
   indentationRules: {
     increaseIndentPattern: new RegExp('(after|else|catch|rescue|fn|^.*(do|<\\-|\\->|\\{|\\[))\\s*$'),
     decreaseIndentPattern: new RegExp('^\\s*((\\}|\\])\\s*$|(after|else|catch|rescue|end)\\b)')
