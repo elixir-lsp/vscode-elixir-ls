ca7bd2c14963622c3edbc199cfd93e1d020cdc85
belaustegui <cristian.alvb@gmail.com>
2019-01-12 15:53:55 +0100

    Improve declarative Elixir configuration
    The declarative configuration for Elixir language has been renamed to
    end with `language-configuration.json`. This enables auto-completion and
    validation in VSCode.
    
    The declarative configuration has been also improved by avoiding
    autoclosing of quotes in strings and comments.
    
    The indentation patterns are now declaratively configured, so the old
    programmatic configuration file has been removed.

--- src/configuration.ts
+++
@@ -1,8 +0,0 @@
-import * as vscode from 'vscode';
-
-export const configuration: vscode.LanguageConfiguration = {
-  indentationRules: {
-    increaseIndentPattern: new RegExp('(after|else|catch|rescue|fn|^.*(do|<\\-|\\->|\\{|\\[|\\=))\\s*$'),
-    decreaseIndentPattern: new RegExp('^\\s*((\\}|\\])\\s*$|(after|else|catch|rescue|end)\\b)')
-  }
-};
