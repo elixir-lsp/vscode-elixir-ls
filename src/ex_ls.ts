e852acbe4034fde7c666fdb1f7e83f7aeae1137b
Norbert Melzer <timmelzer@gmail.com>
2019-01-19 15:14:32 +0100

    fix lints

--- src/ex_ls.ts
+++ src/ex_ls.ts
@@ -1,18 +1,16 @@
-import { ServerOptions } from "vscode-languageclient";
-import { ExtensionContext, window } from "vscode";
-import { join as pathJoin } from "path";
-import { platform } from "os";
-import { mkdirSync, access, writeFile } from "fs";
-import { get } from "https";
-import { Repository } from "gitty";
 import { spawnSync } from "child_process";
+import { Repository } from "gitty";
+import { platform } from "os";
+import { join as pathJoin } from "path";
+import { ExtensionContext, window } from "vscode";
+import { ServerOptions } from "vscode-languageclient";
 
 // TODO: make this configurable
 // This ZIP is directly specifying a commit at github
 const sourceRepo: string = "https://github.com/elixir-lsp/elixir-ls.git";
 
 export function configureElixirLS(
-  context: ExtensionContext
+  context: ExtensionContext,
 ): Thenable<ServerOptions> {
   const cmd: string = "language_server" + file_type();
   const repoPath: string = pathJoin(context.extensionPath, "elixir-ls-repo");
@@ -21,11 +19,11 @@ export function configureElixirLS(
 
   const opts = { command: fullCmd };
   const serverOpts: ServerOptions = {
+    debug: opts,
     run: opts,
-    debug: opts
   };
 
-  let result: Promise<ServerOptions> = new Promise<"update" | "cloned">(
+  const result: Promise<ServerOptions> = new Promise<"update" | "cloned">(
     (resolve, reject) => {
       Repository.clone(repoPath, sourceRepo, (err: any) => {
         if (!err) {
@@ -36,21 +34,21 @@ export function configureElixirLS(
           reject(err);
         }
       });
-    }
+    },
   )
     .then((action) => {
-      if (action == "update") {
+      if (action === "update") {
         // TODO: check for updates
       }
 
       // TODO: check errors
       // TODO: skip conditionally
-      spawnSync('mix', ['deps.get'], { cwd: repoPath })
-      spawnSync('mix', ['compile'], { cwd: repoPath });
-      spawnSync('mix', ['elixir_ls.release', '-o', path], { cwd: repoPath });
+      spawnSync("mix", ["deps.get"], { cwd: repoPath });
+      spawnSync("mix", ["compile"], { cwd: repoPath });
+      spawnSync("mix", ["elixir_ls.release", "-o", path], { cwd: repoPath });
     })
-    .then(_ => serverOpts)
-    .catch(err => {
+    .then((_) => serverOpts)
+    .catch((err) => {
       window.showErrorMessage(`${err}`);
       return null;
     });
@@ -59,7 +57,7 @@ export function configureElixirLS(
 }
 
 function file_type(): ".sh" | ".bat" {
-  if (platform() == "win32") {
+  if (platform() === "win32") {
     return ".bat";
   } else {
     return ".sh";
--- src/extension.ts
+++ src/extension.ts
@@ -4,22 +4,12 @@
  * ------------------------------------------------------------------------------------------ */
 "use strict";
 
-import * as vscode from "vscode";
-import { configuration } from "./configuration";
 import { execSync } from "child_process";
 import * as shell from "shelljs";
-
-import { workspace, Disposable, ExtensionContext } from "vscode";
-import {
-  LanguageClient,
-  LanguageClientOptions,
-  RevealOutputChannelOn,
-  SettingMonitor,
-  ServerOptions,
-  TransportKind
-} from "vscode-languageclient";
-import { platform } from "os";
-
+import * as vscode from "vscode";
+import { ExtensionContext, workspace } from "vscode";
+import { LanguageClient, LanguageClientOptions, RevealOutputChannelOn, ServerOptions } from "vscode-languageclient";
+import { configuration } from "./configuration";
 import { configureElixirLS } from "./ex_ls";
 
 export function activate(context: ExtensionContext) {
@@ -28,11 +18,11 @@ export function activate(context: ExtensionContext) {
   const serverOpts: Thenable<ServerOptions> = configureElixirLS(context);
 
   // Options to control the language client
-  let clientOptions: LanguageClientOptions = {
+  const clientOptions: LanguageClientOptions = {
     // Register the server for Elixir documents
     documentSelector: [
       { language: "elixir", scheme: "file" },
-      { language: "elixir", scheme: "untitled" }
+      { language: "elixir", scheme: "untitled" },
     ],
     // Don't focus the Output pane on errors because request handler errors are no big deal
     revealOutputChannelOn: RevealOutputChannelOn.Never,
@@ -41,20 +31,22 @@ export function activate(context: ExtensionContext) {
       configurationSection: "elixirLS",
       // Notify the server about file changes to Elixir files contained in the workspace
       fileEvents: [
-        workspace.createFileSystemWatcher("**/*.{ex,exs,erl,yrl,xrl,eex}")
-      ]
-    }
+        workspace.createFileSystemWatcher("**/*.{ex,exs,erl,yrl,xrl,eex}"),
+      ],
+    },
   };
 
   // Create the language client and start the client.
-  serverOpts.then((serverOpts) => {
-    if (serverOpts === null) { return }
+  serverOpts.then((serverOptions) => {
+    if (serverOptions === null) {
+      return;
+    }
 
-    let disposable = new LanguageClient(
+    const disposable = new LanguageClient(
       "ElixirLS",
       "ElixirLS",
-      serverOpts,
-      clientOptions
+      serverOptions,
+      clientOptions,
     ).start();
 
     // Push the disposable to the context's subscriptions so that the
@@ -62,12 +54,12 @@ export function activate(context: ExtensionContext) {
     context.subscriptions.push(disposable);
 
     context.subscriptions.push(
-      vscode.languages.setLanguageConfiguration("elixir", configuration)
+      vscode.languages.setLanguageConfiguration("elixir", configuration),
     );
-  })
+  });
 }
 
-function testElixirCommand(command: String) {
+function testElixirCommand(command: string) {
   try {
     return execSync(`${command} -e ""`);
   } catch {
@@ -76,7 +68,7 @@ function testElixirCommand(command: String) {
 }
 
 function testElixir() {
-  var testResult = testElixirCommand("elixir");
+  let testResult = testElixirCommand("elixir");
   if (testResult === false) {
     // Try finding elixir in the path directly
     const elixirPath = shell.which("elixir");
@@ -87,20 +79,20 @@ function testElixir() {
 
   if (!testResult) {
     vscode.window.showErrorMessage(
-      "Failed to run 'elixir' command. ElixirLS will probably fail to launch. Logged PATH to Development Console."
+      "Failed to run 'elixir' command. ElixirLS will probably fail to launch. Logged PATH to Development Console.",
     );
     console.warn(
       `Failed to run 'elixir' command. Current process's PATH: ${
-        process.env["PATH"]
-      }`
+        process.env.PATH
+      }`,
     );
     return false;
   } else if (testResult.length > 0) {
     vscode.window.showErrorMessage(
-      "Running 'elixir' command caused extraneous print to stdout. See VS Code's developer console for details."
+      "Running 'elixir' command caused extraneous print to stdout. See VS Code's developer console for details.",
     );
     console.warn(
-      "Running 'elixir -e \"\"' printed to stdout:\n" + testResult.toString()
+      "Running 'elixir -e \"\"' printed to stdout:\n" + testResult.toString(),
     );
     return false;
   } else {
