0de19e86f4949d75c4b5fc7f3b7baf4f8a9f6c54
niku <niku@niku.name>
2020-01-04 17:32:05 +0900

    Fix reports by lint
    before the change:
    
    ```
    $ npx eslint . --ext .js,.jsx,.ts,.tsx --fix
    
    /Users/niku/src/vscode-elixir-ls/src/extension.ts
      20:8  warning  Missing return type on function              @typescript-eslint/explicit-function-return-type
      21:3  error    'testElixir' was used before it was defined  @typescript-eslint/no-use-before-define
      69:1  warning  Missing return type on function              @typescript-eslint/explicit-function-return-type
      77:1  warning  Missing return type on function              @typescript-eslint/explicit-function-return-type
    
    /Users/niku/src/vscode-elixir-ls/src/test/runTest.ts
      5:1  warning  Missing return type on function  @typescript-eslint/explicit-function-return-type
    
    ✖ 5 problems (1 error, 4 warnings)
    ```
    
    after the change:
    
    ```
    ❯ npx eslint . --ext .js,.jsx,.ts,.tsx
    
    ```

--- src/extension.ts
+++ src/extension.ts
@@ -17,7 +17,46 @@ import {
 } from "vscode-languageclient";
 import { platform } from "os";
 
-export function activate(context: ExtensionContext) {
+function testElixirCommand(command: string): false | Buffer {
+  try {
+    return execSync(`${command} -e ""`);
+  } catch {
+    return false;
+  }
+}
+
+function testElixir(): boolean {
+  let testResult = testElixirCommand("elixir");
+  if (testResult === false) {
+    // Try finding elixir in the path directly
+    const elixirPath = shell.which("elixir");
+    if (elixirPath) {
+      testResult = testElixirCommand(elixirPath);
+    }
+  }
+
+  if (!testResult) {
+    vscode.window.showErrorMessage(
+      "Failed to run 'elixir' command. ElixirLS will probably fail to launch. Logged PATH to Development Console."
+    );
+    console.warn(
+      `Failed to run 'elixir' command. Current process's PATH: ${process.env["PATH"]}`
+    );
+    return false;
+  } else if (testResult.length > 0) {
+    vscode.window.showErrorMessage(
+      "Running 'elixir' command caused extraneous print to stdout. See VS Code's developer console for details."
+    );
+    console.warn(
+      "Running 'elixir -e \"\"' printed to stdout:\n" + testResult.toString()
+    );
+    return false;
+  } else {
+    return true;
+  }
+}
+
+export function activate(context: ExtensionContext): void {
   testElixir();
 
   const command =
@@ -64,43 +103,4 @@ export function activate(context: ExtensionContext) {
   // Push the disposable to the context's subscriptions so that the
   // client can be deactivated on extension deactivation
   context.subscriptions.push(disposable);
-}
-
-function testElixirCommand(command: string) {
-  try {
-    return execSync(`${command} -e ""`);
-  } catch {
-    return false;
-  }
-}
-
-function testElixir() {
-  let testResult = testElixirCommand("elixir");
-  if (testResult === false) {
-    // Try finding elixir in the path directly
-    const elixirPath = shell.which("elixir");
-    if (elixirPath) {
-      testResult = testElixirCommand(elixirPath);
-    }
-  }
-
-  if (!testResult) {
-    vscode.window.showErrorMessage(
-      "Failed to run 'elixir' command. ElixirLS will probably fail to launch. Logged PATH to Development Console."
-    );
-    console.warn(
-      `Failed to run 'elixir' command. Current process's PATH: ${process.env["PATH"]}`
-    );
-    return false;
-  } else if (testResult.length > 0) {
-    vscode.window.showErrorMessage(
-      "Running 'elixir' command caused extraneous print to stdout. See VS Code's developer console for details."
-    );
-    console.warn(
-      "Running 'elixir -e \"\"' printed to stdout:\n" + testResult.toString()
-    );
-    return false;
-  } else {
-    return true;
-  }
 }
