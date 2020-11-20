56eb801fdd3b12a3b6e85c4e22ca9ee7c0ca18bd
Étienne Lévesque <etienne.levesque16@gmail.com>
2020-11-19 20:24:21 -0500

    Remove dead code

--- src/constants/commands.ts
+++ src/constants/commands.ts
@@ -1,5 +1,3 @@
 export default {
-  EXECUTE_WORKSPACE_COMMAND: 'elixir.execute.workspaceCommand',
-  SEARCH_TEST_CODE_LENS: 'vscode.elixir.test.search.codelens',
   RUN_TEST_FROM_CODELENS: 'elixir.test.run'
 }
