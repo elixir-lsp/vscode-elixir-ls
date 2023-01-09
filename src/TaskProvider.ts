fbad6a86ff6b561b53f1bcc1ddff7fdc5e27ff55
Lukasz Samson <lukaszsamson@gmail.com>
2023-01-09 23:59:55 +0100

    add more tasks

--- src/TaskProvider.ts
+++ src/TaskProvider.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 
 export class TaskProvider implements vscode.TaskProvider {
     // Referenced in package.json::taskDefinitions
-    static TaskType = 'ElixirLS';
+    static TaskType = 'mix';
 
     public provideTasks(): vscode.Task[] {
         const wsFolders = vscode.workspace.workspaceFolders;
@@ -11,10 +11,39 @@ export class TaskProvider implements vscode.TaskProvider {
             return [];
         }
 
-        const kind: vscode.TaskDefinition = { type: TaskProvider.TaskType }
+        // TODO make sure that problem matchers are working
+        // TODO better handle multi root workspaces
+
+        const tasks = [];
+
+        const test = new vscode.Task(
+            { type: TaskProvider.TaskType, task: 'Run tests' },
+            wsFolders[0],
+            'Run tests',
+            TaskProvider.TaskType,
+            new vscode.ShellExecution("mix test"),
+            ["$mixCompileError", "$mixCompileWarning", "$mixTestFailure"]
+        );
+
+        test.group = vscode.TaskGroup.Test;
+
+        tasks.push(test);
+
+        const testCoverage = new vscode.Task(
+            { type: TaskProvider.TaskType, task: 'Run tests with coverage' },
+            wsFolders[0],
+            'Run tests with coverage',
+            TaskProvider.TaskType,
+            new vscode.ShellExecution("mix test.coverage"),
+            ["$mixCompileError", "$mixCompileWarning", "$mixTestFailure"]
+        );
+
+        testCoverage.group = vscode.TaskGroup.Test;
+
+        tasks.push(testCoverage);
 
         const testUnderCursorTask = new vscode.Task(
-            kind,
+            { type: TaskProvider.TaskType, task: 'Run test at cursor' },
             wsFolders[0],
             'Run test at cursor',
             TaskProvider.TaskType,
@@ -24,8 +53,10 @@ export class TaskProvider implements vscode.TaskProvider {
 
         testUnderCursorTask.group = vscode.TaskGroup.Test;
 
+        tasks.push(testUnderCursorTask);
+
         const testsInFileTask = new vscode.Task(
-            kind,
+            { type: TaskProvider.TaskType, task: 'Run tests in current file' },
             wsFolders[0],
             'Run tests in current file',
             TaskProvider.TaskType,
@@ -35,7 +66,183 @@ export class TaskProvider implements vscode.TaskProvider {
 
         testsInFileTask.group = vscode.TaskGroup.Test;
 
-        return [testUnderCursorTask, testsInFileTask];
+        tasks.push(testsInFileTask);
+
+        const compile = new vscode.Task(
+            { type: TaskProvider.TaskType, task: 'Build' },
+            wsFolders[0],
+            'Build',
+            TaskProvider.TaskType,
+            new vscode.ShellExecution("mix compile"),
+            ["$mixCompileError", "$mixCompileWarning"]
+        );
+
+        compile.group = vscode.TaskGroup.Build;
+
+        tasks.push(compile);
+
+        const depsCompile = new vscode.Task(
+            { type: TaskProvider.TaskType, task: 'Build dependencies' },
+            wsFolders[0],
+            'Build dependencies',
+            TaskProvider.TaskType,
+            new vscode.ShellExecution("mix deps.compile"),
+            ["$mixCompileError", "$mixCompileWarning"]
+        );
+
+        depsCompile.group = vscode.TaskGroup.Build;
+
+        tasks.push(depsCompile);
+
+        const clean = new vscode.Task(
+            { type: TaskProvider.TaskType, task: 'Clean project' },
+            wsFolders[0],
+            'Clean project',
+            TaskProvider.TaskType,
+            new vscode.ShellExecution("mix clean")
+        );
+
+        clean.group = vscode.TaskGroup.Clean;
+
+        tasks.push(clean);
+
+        const cleanWithDeps = new vscode.Task(
+            { type: TaskProvider.TaskType, task: 'Clean project and deps' },
+            wsFolders[0],
+            'Clean project and deps',
+            TaskProvider.TaskType,
+            new vscode.ShellExecution("mix clean --deps")
+        );
+
+        cleanWithDeps.group = vscode.TaskGroup.Clean;
+
+        tasks.push(cleanWithDeps);
+
+        const appTree = new vscode.Task(
+            { type: TaskProvider.TaskType, task: 'Print app tree' },
+            wsFolders[0],
+            'Print app tree',
+            TaskProvider.TaskType,
+            new vscode.ShellExecution("mix app.tree")
+        );
+
+        tasks.push(appTree);
+
+        const deps = new vscode.Task(
+            { type: TaskProvider.TaskType, task: 'List deps' },
+            wsFolders[0],
+            'List deps',
+            TaskProvider.TaskType,
+            new vscode.ShellExecution("mix deps")
+        );
+
+        tasks.push(deps);
+
+        const depsCleanAll = new vscode.Task(
+            { type: TaskProvider.TaskType, task: 'Clean all deps' },
+            wsFolders[0],
+            'Clean all deps',
+            TaskProvider.TaskType,
+            new vscode.ShellExecution("mix deps.clean --all")
+        );
+
+        depsCleanAll.group = vscode.TaskGroup.Clean;
+
+        tasks.push(depsCleanAll);
+
+        const depsCleanUnused = new vscode.Task(
+            { type: TaskProvider.TaskType, task: 'Clean all unused deps' },
+            wsFolders[0],
+            'Clean all unused deps',
+            TaskProvider.TaskType,
+            new vscode.ShellExecution("mix deps.clean --unlock --unused")
+        );
+
+        depsCleanUnused.group = vscode.TaskGroup.Clean;
+
+        tasks.push(depsCleanUnused);
+
+        const depsGet = new vscode.Task(
+            { type: TaskProvider.TaskType, task: 'Get deps' },
+            wsFolders[0],
+            'Get deps',
+            TaskProvider.TaskType,
+            new vscode.ShellExecution("mix deps.get")
+        );
+
+        tasks.push(depsGet);
+
+        const depsUpdateAll = new vscode.Task(
+            { type: TaskProvider.TaskType, task: 'Update all deps' },
+            wsFolders[0],
+            'Update all deps',
+            TaskProvider.TaskType,
+            new vscode.ShellExecution("mix deps.update --all")
+        );
+
+        tasks.push(depsUpdateAll);
+
+        const format = new vscode.Task(
+            { type: TaskProvider.TaskType, task: 'Format' },
+            wsFolders[0],
+            'Format',
+            TaskProvider.TaskType,
+            new vscode.ShellExecution("mix format")
+        );
+
+        tasks.push(format);
+
+        const run = new vscode.Task(
+            { type: TaskProvider.TaskType, task: 'Run' },
+            wsFolders[0],
+            'Run',
+            TaskProvider.TaskType,
+            new vscode.ShellExecution("mix run")
+        );
+
+        tasks.push(run);
+
+        const runNoHalt = new vscode.Task(
+            { type: TaskProvider.TaskType, task: 'Run no halt' },
+            wsFolders[0],
+            'Run no halt',
+            TaskProvider.TaskType,
+            new vscode.ShellExecution("mix run --no-halt")
+        );
+
+        tasks.push(runNoHalt);
+
+        const releaseInit = new vscode.Task(
+            { type: TaskProvider.TaskType, task: 'Generates sample files for releases' },
+            wsFolders[0],
+            'Generates sample files for releases',
+            TaskProvider.TaskType,
+            new vscode.ShellExecution("mix release.init")
+        );
+
+        tasks.push(releaseInit);
+
+        const xrefTraceFile = new vscode.Task(
+            { type: TaskProvider.TaskType, task: 'Trace file dependencies' },
+            wsFolders[0],
+            'Trace file dependencies',
+            TaskProvider.TaskType,
+            new vscode.ShellExecution("mix xref trace ${relativeFile}")
+        );
+
+        tasks.push(xrefTraceFile);
+
+        const xrefGraph = new vscode.Task(
+            { type: TaskProvider.TaskType, task: 'Print file dependency graph' },
+            wsFolders[0],
+            'Print file dependency graph',
+            TaskProvider.TaskType,
+            new vscode.ShellExecution("mix xref graph")
+        );
+
+        tasks.push(xrefGraph);
+
+        return tasks;
     }
 
     public resolveTask(_task: vscode.Task): vscode.Task | undefined {
