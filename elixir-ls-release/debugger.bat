05385cfa3672053734a3f59692ff8e6b6c128bc8
Lukasz Samson <lukaszsamson@gmail.com>
2023-04-07 01:26:29 +0200

    make windows scripts work

--- elixir-ls-release/debugger.bat
+++ elixir-ls-release/debugger.bat
@@ -2,9 +2,14 @@
 
 SET ELS_MODE=debugger
 IF EXIST "%APPDATA%\elixir_ls\setup.bat" (
-    CALL "%APPDATA%\elixir_ls\setup.bat" > nul
+    ECHO "" | CALL "%APPDATA%\elixir_ls\setup.bat" > nul 2>&1
+    IF %ERRORLEVEL% NEQ 0 EXIT 1
 )
 
 SET MIX_ENV=prod
-elixir "%~dp0\quiet_install.exs" > nul
-elixir --erl "+sbwt none +sbwtdcpu none +sbwtdio none" "%~dp0\debugger.exs"
+@REM pipe echo to avoid passing protocol messages to quiet install command
+@REM intercept stdout and stderr
+@REM elixir is a batch script and needs to be called
+ECHO "" | CALL elixir "%~dp0quiet_install.exs" > nul 2>&1
+IF %ERRORLEVEL% NEQ 0 EXIT 1
+elixir --erl "+sbwt none +sbwtdcpu none +sbwtdio none" "%~dp0debugger.exs"
