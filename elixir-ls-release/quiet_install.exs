57b45da32ac81bffe536e8c5a453321c3630ad3f
Lukasz Samson <lukaszsamson@gmail.com>
2023-05-03 14:28:27 +0200

    Put mix into quiet mode to surface git errors
    I checked this on my machine and when I run git fetch without internet it exits with code 128 and prints to stderr
    ssh: connect to host github.com port 22: Undefined error: 0
    fatal: Could not read from remote repository.
    
    Please make sure you have the correct access rights
    and the repository exists.
    However when this is run via Mix.SCM.Git.git! and we are running with Mix.Shell.IO we are getting an %IO.Stream{} here
    https://github.com/elixir-lang/elixir/blob/6f96693b355a9b670f2630fd8e6217b69e325c5a/lib/mix/lib/mix/scm/git.ex#LL304C1-L304C1
    so it’s getting printed to stderr. stderr is redirected to stdout for some reason in git! and hence we are redirecting stdout to /dev/null in elixir-ls launch scripts it’s getting swallowed and only the mix exception error is printed to stderr.
    
    Solution: change mix shell and make the command return binary that gets printed to stderr
    
    Fixes https://github.com/elixir-lsp/vscode-elixir-ls/issues/327

--- elixir-ls-release/quiet_install.exs
+++ elixir-ls-release/quiet_install.exs
@@ -1,3 +1,12 @@
+# Put mix into quiet mode so it does not print anything to standard out
+# especially it makes it surface git command errors such as reported in
+# https://github.com/elixir-lsp/vscode-elixir-ls/issues/320
+# to standard error
+# see implementation in
+# https://github.com/elixir-lang/elixir/blob/6f96693b355a9b670f2630fd8e6217b69e325c5a/lib/mix/lib/mix/scm/git.ex#LL304C1-L304C1
+Mix.start
+Mix.shell(Mix.Shell.Quiet)
+
 if System.get_env("ELS_LOCAL") == "1" do
   dir = Path.expand("#{__DIR__}/../elixir-ls")
   IO.puts(:stderr, "Starting local ElixirLS install from #{dir}")
