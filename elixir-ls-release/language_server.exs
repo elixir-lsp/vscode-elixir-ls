2f88c5128f6dca5f4af63ea053fe8cd31c33b6e8
Lukasz Samson <lukaszsamson@gmail.com>
2023-05-06 12:13:36 +0200

    disable protocol consolidation as it's causing problems in debugger

--- elixir-ls-release/language_server.exs
+++ elixir-ls-release/language_server.exs
@@ -5,11 +5,13 @@ if System.get_env("ELS_LOCAL") == "1" do
       {:elixir_ls, path: dir},
     ],
     config_path: Path.join(dir, "config/config.exs"),
-    lockfile: Path.join(dir, "mix.lock")
+    lockfile: Path.join(dir, "mix.lock"),
+    consolidate_protocols: false
   )
 else
   Mix.install([
-    {:elixir_ls, github: "elixir-lsp/elixir-ls", tag: System.get_env("ELS_RELEASE")}
+    {:elixir_ls, github: "elixir-lsp/elixir-ls", tag: System.get_env("ELS_RELEASE")},
+    consolidate_protocols: false
   ])
 end
 
--- elixir-ls-release/quiet_install.exs
+++ elixir-ls-release/quiet_install.exs
@@ -16,6 +16,7 @@ if System.get_env("ELS_LOCAL") == "1" do
       {:elixir_ls, path: dir},
     ],
     # force: true,
+    consolidate_protocols: false,
     config_path: Path.join(dir, "config/config.exs"),
     lockfile: Path.join(dir, "mix.lock")
   )
@@ -23,6 +24,7 @@ else
   IO.puts(:stderr, "Starting ElixirLS release #{System.get_env("ELS_RELEASE")}")
   IO.puts(:stderr, "Running in #{File.cwd!}")
   Mix.install([
-    {:elixir_ls, github: "elixir-lsp/elixir-ls", tag: System.get_env("ELS_RELEASE")}
+    {:elixir_ls, github: "elixir-lsp/elixir-ls", tag: System.get_env("ELS_RELEASE")},
+    consolidate_protocols: false
   ])
 end
