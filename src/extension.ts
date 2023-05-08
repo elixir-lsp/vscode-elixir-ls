f05e86babfb6336d016e46bab31afc038fddafb7
Lukasz Samson <lukaszsamson@gmail.com>
2023-05-08 22:23:38 +0200

    remove bogus MS license

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,3 @@
-// TODO why we have MS copyright here?
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
 "use strict";
 
 import * as vscode from "vscode";
